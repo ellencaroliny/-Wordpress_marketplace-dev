935aa369b2cfb893a310b06b543551463b995764
Ellen Caroliny <147538580+ellencaroliny@users.noreply.github.com>
2025-12-12 18:16:30 -0300

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-nomeApp="hello-react"
-
-sudo supervisorctl stop $nomeApp
+if pm2 list | grep -q hello-react; then
+  pm2 stop hello-react
+else
+  echo "hello-react não está rodando"
+fi
